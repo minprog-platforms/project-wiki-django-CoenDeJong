d8361197c3397770056ebc3f51f26298d3ca233a
Coen de Jong <84028466+CoenDeJong@users.noreply.github.com>
2022-12-10 13:44:31 +0100

    Minimale specificaties

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("random", views.random, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,95 @@
 from django.shortcuts import render
+import markdown2
+from django import forms
+from random import choice
 
 from . import util
 
+class Searchform(forms.Form):
+    query = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Search Wiki"}))
+
+class NewPage(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput({'placeholder': "Title"}))
 
 def index(request):
+    searchform = Searchform()
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "searchform": searchform
     })
 
+def entry(request, title):
+    """ Redirect to title page if it exists """
+    searchform = Searchform()
+
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/not_found.html", {
+            "searchform" : Searchform()
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "content": markdown2.markdown(util.get_entry(title)),
+        "title": title,
+        "searchform" : searchform
+    })
+
+def search(request):
+    """ Loads a page for what is searched """
+    searchform = Searchform()
+    searchlist = []
+
+    if request.method == "POST":
+        searchform = Searchform(request.POST)
+
+    if searchform.is_valid():
+        query = searchform.cleaned_data["query"]
+
+        if util.get_entry(query):
+            return render(request, "encyclopedia/entry.html", {
+                "content": markdown2.markdown(util.get_entry(query)),
+                "title": query,
+                "searchform" : searchform
+            })
+
+        for entry in util.list_entries():
+            if query.lower() in entry.lower():
+                searchlist.append(entry)
+
+        return render(request, "encyclopedia/searchresults.html", {
+            "searchlist": searchlist,
+            "query": query,
+            "searchform": searchform
+            })
+
+def create(request):
+    """ Allows the user to create a wiki page """
+    searchform = Searchform()
+    createform = NewPage()
+
+    if request.method == "POST":
+        createform = NewPage(request.POST)
+        content = request.POST["content"]
+
+        if createform.is_valid():
+            title = createform.cleaned_data['title']
+
+            if title in util.list_entries():
+                return render(request, "encyclopedia/exists.html", {
+                    "searchform": searchform
+                    })
+
+            util.save_entry(title, content)
+            return entry(request, title)
+    else:
+        form = NewPage()
+        return render(request, "encyclopedia/create.html", {
+            "searchform": searchform,
+            "createform": createform
+            })
+
+def random(request):
+    """ Brings user to a random wiki page """
+    titles = util.list_entries()
+    title = choice(titles)
+    return entry(request, title)
